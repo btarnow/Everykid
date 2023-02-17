55f9bedb63511bbe2fa984d985ad7865e147fad8
btarnow <beckytarnowski@gmail.com>
2023-02-17 18:34:06 -0500

    removed no longer needed code from user_page.js

--- static/js/user_page.js
+++ static/js/user_page.js
@@ -1,12 +1,3 @@
 "use strict"
 
-const createCollectionForm = document.querySelector('#collection-form-label');
-const toggleForm = document.querySelector('#collection-form-div');
 
-createCollectionForm.addEventListener('click', () => {
-    if (toggleForm.hidden === true){
-        toggleForm.hidden = false;
-    } else {
-        toggleForm.hidden = true;
-    }
-})
