97c7a6936f8deb6104b69f4bace24c8a4b2fbaf8
btarnow <beckytarnowski@gmail.com>
2023-04-02 16:26:51 -0400

    changed unit tests to test code from test_database

--- temp_test.py
+++ temp_test.py
@@ -3,51 +3,47 @@ book_info_from_API.py"""
 
 import unittest
 from temp import get_title, get_publish_date, get_cover_url_path, get_overview
+from test_database import test_books_list_dicts, test_books_list_raw
+
+
+test_books_raw = test_books_list_raw()
+test_books_list_of_dicts = test_books_list_dicts()
 
 
 class TempTests(unittest.TestCase):
 
   def test_get_title_works(self):
-      """Test that get_title can find a title entry in a dictionary."""
+      """Tests that get_title can find a title entry in a dictionary."""
 
-      expected_title = "Kindness is My Superpower"
-      test_title_dict = {"title": expected_title}
-
-      self.assertEqual(expected_title, get_title(test_title_dict))
-
+      for index, raw_book in enumerate(test_books_raw):
+          expected_book_dict = test_books_list_of_dicts[index]
+          expected_title = expected_book_dict["title"]
+          
+          with self.subTest():
+              self.assertEqual(expected_title, get_title(raw_book), 
+                               f"{expected_title} was expected")
   
-  def test_get_title_not_found(self):
-      """Test that if a title is not found it returns 'No title found' """
-
-      test_data_without_title = {}
-      expected_not_found_msg = "No title found"
-
-      self.assertEqual(expected_not_found_msg, get_title(test_data_without_title))
-
 
   def test_get_publish_date_returns_year(self):
-    """Test that get_publish_date only returns the published year"""
-    
-    test_dates_in_dict = [{"publish_date": "Jul 27, 2020"}, 
-                          {"publish_date": "2022"}, {}]
-    expected_dates= ["2020", "2022", "0"]
+      """Test that get_publish_date only returns the published year"""
+      
+      for index, raw_book in enumerate(test_books_raw):
+          expected_book_dict = test_books_list_of_dicts[index]
+          expected_year_published = expected_book_dict["year_published"]
 
-    for index, date in enumerate(test_dates_in_dict): 
-        with self.subTest():
-            expected_date = expected_dates[index]
+          with self.subTest():
+              self.assertEqual(expected_year_published, get_publish_date(raw_book), 
+                               f"{expected_year_published} was expected")
+              
 
-        self.assertEqual(expected_date, get_publish_date(date), 
-                     f"Expected date is {expected_date}")
-
-  
   def test_get_cover_url_path(self):
-    """"""
-    # TODO: 
+      """"""
+      # TODO: 
 
   
   def test_get_overview(self):
-    """"""
-    # TODO: 
+      """"""
+      # TODO: 
 
 
 unittest.main()
